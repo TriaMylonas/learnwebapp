380075cd795709fc8c16e25332a86266deebd97f
TriaMylo <t.mylonas@outlook.de>
2023-09-16 20:04:27 +0200

    initial commit

---
+++ src/main/java/dev/triamylo/learnwebapp/LearnwebappApplication.java
@@ -0,0 +1,13 @@
+package dev.triamylo.learnwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnwebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnwebappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/triamylo/learnwebapp/LearnwebappApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.triamylo.learnwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnwebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
