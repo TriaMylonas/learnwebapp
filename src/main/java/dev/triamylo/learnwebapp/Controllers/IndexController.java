fa4214fd0a180d6f6e3bf11c5650d38a24db5e2b
Triantafyllos Mylonas <triantafyllos.mylonas@schnellecke.com>
2023-09-18 15:41:06 +0200

    add explain comments

--- src/main/java/dev/triamylo/learnwebapp/Controllers/IndexController.java
+++ src/main/java/dev/triamylo/learnwebapp/Controllers/IndexController.java
@@ -12,19 +12,35 @@ import org.springframework.web.bind.annotation.PostMapping;
 public class IndexController {
 
 
+    /*get method is when the client ask for the current url and I must (server side) give him
+    * something back. When it is a Controller I must give the client html view back. When its a
+    * RestController I give back Object as JSON in the regel.
+    */
     @GetMapping("/")
     public String startSite(Model model) {
-        User user = new User("", "");
+        // I initialise one object, so that the form in the html can bind with it.
+        User user = new User("tria", "myl");
+        //I pass it to the model (html) through model and attribute name "user"
+        // with "user" obj I have bind my form through Thymeleaf to.
         model.addAttribute("user", user);
+
+        //when the client ask for the root (localhost:8080) I give him bak the index.html view
+        // the spring boot search for this view in the resource/templates/ folder as default.
         return "index";
     }
 
-//    @GetMapping("/")
 
+    /* with post method I take values from the client to server side.
+    * here I take the values that the client put in the form on index.xtml
+    * when he clicks Submit in the form the values will go to the /register url
+    * and I will then start the registerSite() method from this controller.
+    * if there is a need of a logic I will be writing it in a Service class and
+    * call it from this method.
+    */
     @PostMapping("/register")
     public String registerSite(@ModelAttribute("user") User aUser, Model model) {
-
-//        model.addAttribute("User",aUser);
+        // here I take the values from the html and put them in the obj user.
+        model.addAttribute("User",aUser);
         return "register";
     }
 
