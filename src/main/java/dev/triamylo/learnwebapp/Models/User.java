48e1dcd5e7faa8d8a1244425de9218bebe8c7f65
Triantafyllos Mylonas <triantafyllos.mylonas@schnellecke.com>
2023-09-18 07:49:11 +0200

    change the model class name to User

--- src/main/java/dev/triamylo/learnwebapp/Models/User.java
+++ src/main/java/dev/triamylo/learnwebapp/Models/User.java
@@ -1,12 +1,12 @@
 package dev.triamylo.learnwebapp.Models;
 
-public class AGuy {
+public class User {
 
     private String firstName;
     private String lastName;
     private int alter;
 
-    public AGuy(String firstName, String lastName, int alter){
+    public User(String firstName, String lastName, int alter){
         this.firstName = firstName;
         this.lastName = lastName;
         this.alter = alter;
