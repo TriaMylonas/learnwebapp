be3259b56d3a850d077c59d02316496fc307b48b
Triantafyllos Mylonas <triantafyllos.mylonas@schnellecke.com>
2023-09-18 13:05:43 +0200

    Step 3 from Denis Task is done!

--- src/main/java/dev/triamylo/learnwebapp/Controllers/IndexController.java
+++ src/main/java/dev/triamylo/learnwebapp/Controllers/IndexController.java
@@ -1,8 +1,13 @@
 package dev.triamylo.learnwebapp.Controllers;
 
 
+import dev.triamylo.learnwebapp.Models.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class IndexController {
@@ -10,8 +15,19 @@ public class IndexController {
 
 
     @GetMapping("/")
-    public String startSite(){
+    public String startSite(Model model){
+        User user = new User("","");
+        model.addAttribute("user", user);
         return "index";
+    }
+
+//    @GetMapping("/")
+
+    @PostMapping("/register")
+    public String registerSite(@ModelAttribute("user") User aUser, Model model){
+
+//        model.addAttribute("User",aUser);
+        return "register";
     }
 
 
--- src/main/java/dev/triamylo/learnwebapp/Models/User.java
+++ src/main/java/dev/triamylo/learnwebapp/Models/User.java
@@ -5,6 +5,7 @@ public class User {
     private String firstName;
     private String lastName;
 
+
     public User(String firstName, String lastName){
         this.firstName = firstName;
         this.lastName = lastName;
