1577119d109501d48b32e33b69680de38229f9e6
Triantafyllos Mylonas <triantafyllos.mylonas@schnellecke.com>
2023-09-20 12:52:33 +0200

    add new attributes day of birth and height to the user Object.

--- src/main/java/dev/triamylo/learnwebapp/Models/User.java
+++ src/main/java/dev/triamylo/learnwebapp/Models/User.java
@@ -1,14 +1,36 @@
 package dev.triamylo.learnwebapp.Models;
 
+import java.util.Date;
+
 public class User {
 
     private String firstName;
     private String lastName;
+    private Date dob;
+    private double height;
 
 
-    public User(String firstName, String lastName) {
+    public User(String firstName, String lastName, Date dob, double height) {
         this.firstName = firstName;
         this.lastName = lastName;
+        this.dob = dob;
+        this.height = height;
+    }
+
+    public Date getDob() {
+        return dob;
+    }
+
+    public void setDob(Date dob) {
+        this.dob = dob;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
     }
 
     public String getFirstName() {
