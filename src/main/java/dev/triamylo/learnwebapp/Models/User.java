8f0acf43a15b63b289760d32d09cadd004591636
Triantafyllos Mylonas <triantafyllos.mylonas@schnellecke.com>
2023-09-22 11:26:22 +0200

    change the Date type to LocalDate, so I can more easily handle the dates. Add also the @Past annotation to check if the date of birth is in the past.

--- src/main/java/dev/triamylo/learnwebapp/Models/User.java
+++ src/main/java/dev/triamylo/learnwebapp/Models/User.java
@@ -3,6 +3,7 @@ package dev.triamylo.learnwebapp.Models;
 import jakarta.validation.constraints.*;
 import org.springframework.format.annotation.DateTimeFormat;
 
+import java.time.LocalDate;
 import java.util.Date;
 
 public class User {
@@ -11,10 +12,11 @@ public class User {
     @Size(min = 1, max = 10)
     private String firstName;
     private String lastName;
-    @DateTimeFormat(pattern = "yyyy-MM-dd")
-    @Past
+
+
+    @Past(message = "muss in der Vergangenheit sein")
     @NotNull
-    private Date dob;
+    private LocalDate dob;
 
     @Min(1)
     private int height;
@@ -23,18 +25,20 @@ public class User {
 
     }
 
-    public User(String firstName, String lastName, Date dob, int height) {
+    public User(String firstName, String lastName, LocalDate dob, int height) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.dob = dob;
         this.height = height;
     }
 
-    public Date getDob() {
+    public LocalDate getDob() {
         return dob;
     }
 
-    public void setDob(Date dob) {
+
+    public void setDob(LocalDate dob) {
+
         this.dob = dob;
     }
 
