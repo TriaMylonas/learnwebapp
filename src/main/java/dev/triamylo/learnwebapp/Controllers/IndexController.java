e4bf1c8d4cf93facabe13e9f5fa87cea40348f0b
Triantafyllos Mylonas <triantafyllos.mylonas@schnellecke.com>
2023-09-25 16:11:14 +0200

    change here the format of the objects LocalDate and give it to the Model as attribute, so it can be display the way I want.

--- src/main/java/dev/triamylo/learnwebapp/Controllers/IndexController.java
+++ src/main/java/dev/triamylo/learnwebapp/Controllers/IndexController.java
@@ -10,18 +10,20 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class IndexController {
 
 
     /*get method is when the client ask for the current url and I must (server side) give him
-    * something back.
-    * When it is a @Controller I must give the client html view back.
-    * When it is a @RestController I give back Object as JSON in the regel.
-    */
+     * something back.
+     * When it is a @Controller I must give the client html view back.
+     * When it is a @RestController I give back Object as JSON in the regel.
+     */
     @GetMapping("/")
     public String startSite(Model model) {
         // I initialise one object, so that the form in the html can bind with it.
@@ -35,21 +37,58 @@ public class IndexController {
         return "index";
     }
 
+    @GetMapping("/users")
+    public String users(Model model) {
+        List<User> users = new ArrayList<>();
+        for (int i = 0; i < 100; i++) {
+            users.add(new User("firstName " + i, "lastName " + i, LocalDate.now(), 1500 + i));
+        }
+
+        model.addAttribute("users", users);
+        return "user";
+    }
+
 
     /* with post method I take values from the client to server side.
-    * here I take the values that the client put in the form on index.html
-    * when he clicks Submit in the form the values will go to the /register url
-    * and I will then start the registerSite() method from this controller.
-    * if there is a need of a logic I will be writing it in a Service class and
-    * call it from this method.
-    */
+     * here I take the values that the client put in the form on index.html
+     * when he clicks Submit in the form the values will go to the /register url
+     * and I will then start the registerSite() method from this controller.
+     * if there is a need of a logic I will be writing it in a Service class and
+     * call it from this method.
+     */
     @PostMapping("/")
-    public String registerSite(@Valid @ModelAttribute("user") User aUser, BindingResult bindingResult) {
+    public String registerSite(@Valid @ModelAttribute("user") User aUser, BindingResult bindingResult, Model model) {
 
-         if (bindingResult.hasErrors())
+
+        // here in the Controller I can also validate the connection between my objects and the model.
+
+
+        if (aUser.getDob() != null) {
+            // that is the value that is come from the form.
+            int test = aUser.getDob().getYear();
+            if (test < 1980 || test > 2000) {
+
+                System.out.println("The Date passt nicht, ich muss hier etwas tun");
+                // ich habe nicht geschafft eine Richtige Error Objekt zu erstellen und mit den form in
+                // Verbindung zu bringen....
+
+                bindingResult.rejectValue("dob", "error.dob.notValidRange", "Date range is not valid");
+            }
+        }
+
+
+        //checking for errors from the Annotations in the Model. If there is something wrong and the values
+        // from the form can not agree with my model, will initialise BindingResult object.
+        if (bindingResult.hasErrors()) {
             return "index";
-        else
-            return "register";
+        }
+
+        //I change the format of the date form the object here in the controller,
+        //after that I pass it to the model, and he will display it in the website
+        String formatDob = aUser.getDob().format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+        model.addAttribute("formatDob", formatDob);
+        return "register";
+
     }
 
 
