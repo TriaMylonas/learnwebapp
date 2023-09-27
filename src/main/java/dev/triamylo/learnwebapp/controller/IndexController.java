3d42841228c902efa6022a3ecb8d740a5c7f20c6
Triantafyllos Mylonas <triantafyllos.mylonas@schnellecke.com>
2023-09-27 12:31:55 +0200

    add mapping for the delete user, Autowired the userService and clear a little the code.

--- src/main/java/dev/triamylo/learnwebapp/controller/IndexController.java
+++ src/main/java/dev/triamylo/learnwebapp/controller/IndexController.java
@@ -1,23 +1,25 @@
-package dev.triamylo.learnwebapp.Controllers;
+package dev.triamylo.learnwebapp.controller;
 
 
-import dev.triamylo.learnwebapp.Models.User;
+import dev.triamylo.learnwebapp.model.User;
+import dev.triamylo.learnwebapp.service.UserService;
 import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
-import java.text.DecimalFormat;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
 import java.util.List;
 
 @Controller
 public class IndexController {
+
+    @Autowired
+    private UserService userService;
 
 
     /*get method is when the client ask for the current url and I must (server side) give him something back.
@@ -42,15 +44,18 @@ public class IndexController {
      */
     @GetMapping("/users")
     public String users(Model model) {
-        List<User> users = new ArrayList<>();
-        for (int i = 0; i < 100; i++) {
-            users.add(new User("firstName " + i, "lastName " + i, LocalDate.now(), 1500 + i));
-        }
 
-        int usersSize = users.size();
+        List<User> users = userService.list();
+
         model.addAttribute("users", users);
-        model.addAttribute("usersSize", usersSize);
         return "user";
+    }
+
+    @GetMapping("/users/delete/{uuid}")
+    public String delete(@PathVariable String uuid) {
+
+        userService.delete(uuid);
+        return "redirect:/users";
     }
 
 
@@ -66,21 +71,15 @@ public class IndexController {
 
 
         // here in the Controller I can also validate the connection between my objects and the model.
-
-
         if (aUser.getDob() != null) {
-            // that is the value that is come from the form.
+            // that is the value that will come from the form.
             int test = aUser.getDob().getYear();
+
             if (test < 1980 || test > 2000) {
-
-                System.out.println("The Date passt nicht, ich muss hier etwas tun");
-                // ich habe nicht geschafft eine Richtige Error Objekt zu erstellen und mit den form in
-                // Verbindung zu bringen....
-
+                //I create am Error to display in the model after
                 bindingResult.rejectValue("dob", "error.dob.notValidRange", "Date range is not valid");
             }
         }
-
 
         //checking for errors from the Annotations in the Model. If there is something wrong and the values
         // from the form can not agree with my model, will initialise BindingResult object.
@@ -88,16 +87,8 @@ public class IndexController {
             return "index";
         }
 
-        //I change the format of the date form the object here in the controller,
-        //after that I pass it to the model, and he will display it in the website
-        String formatDob = aUser.getDob().format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
-        model.addAttribute("formatDob", formatDob);
-
-        // Format the height with a dot as thousands separator and three decimal places
-        //and add it to the model as String, so it can be displayed from thymeleaf.
-        DecimalFormat decimalFormat = new DecimalFormat("#,###");
-        String formatHeight = decimalFormat.format(aUser.getHeight());
-        model.addAttribute("formatHeight", formatHeight);
+        //I add the new user after the validation to my list of users
+        userService.add(aUser);
         return "register";
 
     }
