ef49b1f1d8aa18576d111f37f28ef052999388b1
Triantafyllos Mylonas <triantafyllos.mylonas@schnellecke.com>
2023-11-03 11:16:13 +0100

    add some commends

--- src/main/java/dev/triamylo/learnwebapp/securityConfiguration/WebSecurityConfig.java
+++ src/main/java/dev/triamylo/learnwebapp/securityConfiguration/WebSecurityConfig.java
@@ -5,6 +5,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractAuthenticationFilterConfigurer;
+import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -19,47 +21,46 @@ import java.util.List;
 public class WebSecurityConfig {
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
-            http
-                    .authorizeHttpRequests((requests) -> requests // SOS that with the images, the same with others object javascript or something like this all unter one ordner so that they can authenticate with one's
-                            .requestMatchers("/","/formula", "/images/**").permitAll() //all can in the home and can add new use
-                            .anyRequest().authenticated()  // all the others musst be authenticated
-                    )
-                    .formLogin((form) -> form  // pos prepei na kanoun anmelden oi  xristes
-                             // das ist die default site from the security
-                            .permitAll() //  gia na mporo ta to xrisimopoiiso xoris na kano login kapou prota
-                    )
-                    .logout((logout) -> logout.permitAll()); // to idio einai kia edo
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
-            return http.build();
+        // SOS --> /images/** --> all unter one ordner so that they can authenticate with one's
+        //all can use the home page and can add new user, all the others musst be authenticated
+        http.authorizeHttpRequests((requests) -> requests
+                        .requestMatchers("/", "/formula", "/images/**").permitAll()
+                        .anyRequest()
+                        .authenticated())
+                .formLogin(AbstractAuthenticationFilterConfigurer::permitAll)  // das ist die default site from the security. (permitAll) I don't need to have permit to access in the login form.
+                .logout(LogoutConfigurer::permitAll); //(permitAll) I don't need to have permit to access in the logout form.
+
+        return http.build();
     }
 
-// use service
+    // use service
 // zwei user mit ADMIN role by spring alle Rolle wird mit GROß
     @Bean
     public UserDetailsService userDetailsService() {
 
         List<UserDetails> users = new ArrayList<>();
 
-        UserDetails user =  User.withDefaultPasswordEncoder()
-                                .username("user")
-                                .password("1")
-                                .roles("USER")
-                                .build();
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("user")
+                .password("1")
+                .roles("USER")
+                .build();
 
         users.add(user);
 
         UserDetails user2 = User.withDefaultPasswordEncoder()
-                                .username("user2")
-                                .password("2")
-                                .roles("ADMIN")
-                                .build();
+                .username("user2")
+                .password("2")
+                .roles("ADMIN")
+                .build();
         users.add(user2);
 
         UserDetails user3 = User.withDefaultPasswordEncoder()
-                                .username("user3")
-                                .password("3")
-                                .build();
+                .username("user3")
+                .password("3")
+                .build();
 
         users.add(user3);
 
