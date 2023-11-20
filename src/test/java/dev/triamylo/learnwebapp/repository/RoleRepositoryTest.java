61ecc79fd0fab5fc68c651e82ad3a5e35a1e1351
Triantafyllos Mylonas <triantafyllos.mylonas@schnellecke.com>
2023-11-20 20:55:11 +0100

    add more test

--- src/test/java/dev/triamylo/learnwebapp/repository/RoleRepositoryTest.java
+++ src/test/java/dev/triamylo/learnwebapp/repository/RoleRepositoryTest.java
@@ -36,9 +36,9 @@ class RoleRepositoryTest extends AbstractApplicationTests {
 
     @Test
     @Transactional  //richtig!!!
-    void addNewRole(){
+    void addNewRoleInTheDatabase() {
 
-        Role testRole= new Role();
+        Role testRole = new Role();
         assertNull(testRole.getUuid());
 
         testRole.setRoleName("testname");
@@ -50,30 +50,64 @@ class RoleRepositoryTest extends AbstractApplicationTests {
 
         assertEquals("testname", optionalRole.get().getRoleName());
         assertNotNull(optionalRole.get().getUuid());
+    }
 
+    @Test
+    @Transactional
+    void addOneNewRoleInOneNewUser(){
 
+        //save role in the DB and check it
+        Role testRole = new Role();
+        assertNull(testRole.getUuid());
+        testRole.setRoleName("roleName");
+        roleRepository.save(testRole);
+        Optional<Role>  optionalRole = roleRepository.findByRoleName("roleName");
+        assertTrue(optionalRole.isPresent());
+
+        //save user in the DB and check it
         User testUser = getNewUser();
-        testUser.setFirstName("testFirstName");
+        testUser.setFirstName("userFirstName");
         testUser.setUuid(null);
-
-        List<Role> roles = new ArrayList<>();
-        roles.add(testRole);
-
-        testUser.setRoles(roles);
-
-        //user speicher
         userRepository.save(testUser);
-
-        Optional<User> optionalUser = userRepository.findByFirstName("testFirstName");
+        Optional<User> optionalUser = userRepository.findByFirstName("userFirstName");
         assertTrue(optionalUser.isPresent());
 
-        assertNotNull(optionalUser.get().getRoles());
+        // add the role in a List and save it to the role object
+        List<Role> roles = new ArrayList<>();
+        roles.add(testRole);
+        testUser.setRoles(roles);
 
+        //save user with roles in DB
+        userRepository.save(testUser);
+        //check if the user have the roles
+        Optional<User> optionalUserWithRoles = userRepository.findByFirstName("userFirstName");
+        assertTrue(optionalUserWithRoles.isPresent());
+        assertNotNull(optionalUserWithRoles.get().getRoles());
         assertEquals(1, optionalUser.get().getRoles().size());
-        // ist meine Role drinnen.
         assertTrue(optionalUser.get().getRoles().contains(testRole));
     }
 
+    @Test
+    @Transactional
+    void addManyRolesInOneNewUser(){
 
+        //create the roles and saved them to DB
+        Role role1 = new Role();
+        role1.setRoleName("user");
+        roleRepository.save(role1);
+
+        Role role2 = new Role();
+
+        role2.setRoleName("admin");
+        Role role3 = new Role();
+        role3.setRoleName("none");
+
+
+        //add the roles in a List
+        //create a user add him the roles List and save him to the DB
+
+        //after check if he has the roles
+
+    }
 
 }
